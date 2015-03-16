82bba82e0c0c66ee2794ad8d11409292493bf4ce
Jayden Navarro <jdndeveloper@gmail.com>
2015-03-15 18:29:18 -0700

    Sorting files created along with the sorting test

---
+++ src/sorts/MergeSort.java
@@ -0,0 +1,7 @@
+package sorts;
+
+/**
+ * Created by Jayden Navarro on 3/15/2015.
+ */
+public class MergeSort {
+}
---
+++ tests/sorttests/SortingTest.java
@@ -0,0 +1,7 @@
+package sorttests;
+
+/**
+ * Created by Jayden Navarro on 3/15/2015.
+ */
+public class SortingTest {
+}
