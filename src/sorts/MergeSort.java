e4d5b0d2ff0b372b0745e2107a09ed68f038a3f8
Jayden Navarro <jdndeveloper@gmail.com>
2015-03-15 18:46:07 -0700

    Added framework to the source files

--- src/sorts/MergeSort.java
+++ src/sorts/MergeSort.java
@@ -1,7 +1,13 @@
 package sorts;
 
+import java.util.ArrayList;
+
 /**
  * Created by Jayden Navarro on 3/15/2015.
  */
 public class MergeSort {
+
+    public static void sort(ArrayList<Integer> theArray) {
+
+    }
 }
--- tests/sorttests/SortingTest.java
+++ tests/sorttests/SortingTest.java
@@ -1,7 +1,43 @@
 package sorttests;
 
+import sorts.BubbleSort;
+import sorts.MergeSort;
+import sorts.QuickSort;
+import sorts.SelectionSort;
+
+import java.util.ArrayList;
+
 /**
  * Created by Jayden Navarro on 3/15/2015.
  */
 public class SortingTest {
+
+    private enum SortingMethod {
+        BUBBLE, MERGE, QUICK, SELECTION
+    }
+
+    private boolean runSort(ArrayList<Integer> theArray, SortingMethod sortingMethod) {
+        switch (sortingMethod) {
+            case BUBBLE:
+                BubbleSort.sort(theArray);
+                break;
+            case MERGE:
+                MergeSort.sort(theArray);
+                break;
+            case QUICK:
+                QuickSort.sort(theArray);
+                break;
+            case SELECTION:
+                SelectionSort.sort(theArray);
+                break;
+            default:
+                return false;
+        }
+
+        return true;
+    }
+
+    private ArrayList<Integer> generateTestArray() {
+        return new ArrayList<Integer>();
+    }
 }
